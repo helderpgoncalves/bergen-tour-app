cbb4fc16596ec1151cc8082911458132a601fc05
Hélder Gonçalves <helderpimentaxc@gmail.com>
2022-05-09 17:01:04 +0100

    Initial Commit

--- App.js
+++ App.js
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions,Image, Button, Alert, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const openPrivacyPolicy = () => {
+    Alert.alert('Privacy Policy', 'This app does not collect any personal information.');
+  }
+
+  const openTermsOfService = () => {
+    Alert.alert('Terms of Service', 'This app does not collect any personal information.');
+  }
+
+  const [markers, setMarkers] = React.useState([{
+    coordinate: {
+      latitude: -34.9290,
+      longitude: 138.6010
+    },
+    title: 'Marker 1',
+    description: 'This is the first marker'
+  }, {
+    coordinate: {
+      latitude: -34.9290,
+      longitude: 138.6010
+    },
+    title: 'Marker 2',
+    description: 'This is the second marker'
+  }]);
+
+  const [region, setRegion] = React.useState({
+    latitude: -34.9290,
+    longitude: 138.6010,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const [languages, setLanguages] = React.useState([{
+    name: 'English',
+    image: require('./assets/icon.png'),
+  }, {
+    name: 'French',
+    image: require('./assets/icon.png'),
+  }]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        {languages.map((language, index) => {
+          return (
+            <Image key={index} style={styles.language} source={language.image} />
+          )
+        }
+        )}
+      </View>
+      <TouchableOpacity style={styles.button} onPress={openPrivacyPolicy}><Text style={{color: "white", fontSize: 24}}>Start the Tour</Text></TouchableOpacity>
+      <MapView region={region}
+       style={styles.map}>
+        {markers.map((marker, index) => (
+          <MapView.Marker
+            key={index}
+            coordinate={marker.coordinate}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+       </MapView>
+       <TouchableOpacity onPress={() => openTermsOfService()}><Text style={styles.termsOfService}>Terms of service</Text></TouchableOpacity>
+       <TouchableOpacity onPress={() => openPrivacyPolicy()}><Text style={styles.privacyPolicy}>Privacy Policy</Text></TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +77,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#36489e',
+    borderRadius: 5,
+    padding: 10,
+    width: Dimensions.get('window').width - 100,
+    alignItems: 'center'
+  },
+  termsOfService: {
+    width: Dimensions.get('window').width,
+    fontSize: 12,
+    color:'#36489e',
+    textAlign: 'right',
+    marginTop: 50,
+    marginRight: 18,
+  },
+  privacyPolicy: {
+    width: Dimensions.get('window').width,
+    fontSize: 12,
+    color:'#36489e',
+    textAlign: 'right',
+    marginTop: 10,
+    marginRight: 18,
+  },
+  map: {
+    top: 30,
+    width: Dimensions.get('window').width - 18,
+    height: Dimensions.get('window').height - 200,
   },
 });
